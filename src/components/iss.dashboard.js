8ccdef497b0b598fa845e71623f76b03566bc53f
vladimir <vladimirsostaric3390@gmail.com>
2019-05-05 21:28:12 +0200

    fix marker not showing

--- src/components/iss.dashboard.js
+++ src/components/iss.dashboard.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react';
 import PropTypes from "prop-types"
-import "../../node_modules/leaflet/dist/leaflet.css"
+import "leaflet/dist/leaflet.css"
 import "./iss.dashboard.css"
 import {Map, Marker, Popup, TileLayer} from "react-leaflet";
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 class IssDashboard extends Component {
 
