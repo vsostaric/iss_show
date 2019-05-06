712e24f22b3996e317027e715dcee267805ac44e
vladimir <vladimirsostaric3390@gmail.com>
2019-05-06 16:26:34 +0200

    implement closest service + add dockerfile - this to complete

--- src/services/iss.service.js
+++ src/services/iss.service.js
@@ -10,5 +10,27 @@ export const fetchIssPosition = () => {
 };
 
 export const findYearWithClosestMeteor = (position, meteor_data) => {
-    return "1966";
+
+    let year = 0;
+    let minDistance = Infinity;
+
+    meteor_data.filter(data => data.geolocation && data.geolocation.coordinates)
+        .forEach(data => {
+            const meteorGeolocationCoordinates = data.geolocation.coordinates;
+            const distance = calculateDistance(
+                position.latitude, position.longitude,
+                meteorGeolocationCoordinates[0], meteorGeolocationCoordinates[1]
+            );
+
+            if (distance < minDistance) {
+                minDistance = distance;
+                year = new Date(data.year).getFullYear().toString();
+            }
+        });
+
+    return year;
+};
+
+const calculateDistance = (x1, y1, x2, y2) => {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
