0c274c8562966f663a768bf1d8eab3de9d47c5dd
vladimir <vladimirsostaric3390@gmail.com>
2019-05-06 12:24:59 +0200

    d3 meteor data graph initial

--- src/components/meteor.data.js
+++ src/components/meteor.data.js
@@ -1,21 +1,61 @@
 import React, {Component} from 'react';
 import PropTypes from "prop-types";
+import rd3 from 'react-d3-library';
+import "./meteor.data.css"
+import * as d3 from "d3";
 
 class MeteorData extends Component {
 
     constructor(props, context) {
         super(props, context);
-
         this.props.refreshMeteorData();
     }
 
-    render() {
-        if (!this.props.meteor_data) {
-            return ""
-        }
-        return "hi";
+    componentDidMount() {
+        this.drawBarChart();
     }
 
+    render() {
+
+        return (
+            <div className="layout">
+                <div className="container">
+                    <svg/>
+                </div>
+            </div>
+        );
+    }
+
+    drawBarChart() {
+
+        const svgWidth = 500;
+        const svgHeight = 300;
+        const svg = d3.select('svg')
+            .attr("width", svgWidth)
+            .attr("height", svgHeight)
+            .attr("class", "bar-chart");
+
+        const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+        const barPadding = 5;
+        const barWidth = (svgWidth / dataset.length);
+        const barChart = svg.selectAll("rect")
+            .data(dataset)
+            .enter()
+            .append("rect")
+            .attr("y", function (d) {
+                return svgHeight - d
+            })
+            .attr("height", function (d) {
+                return d;
+            })
+            .attr("width", barWidth - barPadding)
+            .attr("transform", function (d, i) {
+                var translate = [barWidth * i, 0];
+                return "translate(" + translate + ")";
+            });
+
+    }
 }
 
 MeteorData.propTypes = {
